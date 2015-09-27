9418d0972ad968a7e22bec6bba5e7a0df403707b
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-27 18:46:38 -0300

    get last three urls from history

--- app/src/main/java/mlwalter/com/browsewithme/BrowseWithMeActivity.java
+++ app/src/main/java/mlwalter/com/browsewithme/BrowseWithMeActivity.java
@@ -1,11 +1,14 @@
 package mlwalter.com.browsewithme;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class BrowseWithMeActivity extends AppCompatActivity {
 
@@ -14,19 +17,33 @@ public class BrowseWithMeActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		doStuff();
+		setContentView(R.layout.activity_browse_with_me);
+		history();
 	}
 
-	private void doStuff() {
+	private void history() {
 		String[] proj = new String[]{BookmarkColumns.TITLE, BookmarkColumns.URL};
 		String sel = BookmarkColumns.BOOKMARK + " = 0"; // 0 = history, 1 = bookmark
 		Cursor cursor = getContentResolver().query(BOOKMARKS_URI, proj, sel, null, null);
-
 		if (cursor.moveToFirst() && cursor.getCount() > 0) {
-			String title = cursor.getString(cursor.getColumnIndex(BookmarkColumns.TITLE));
-			String url = cursor.getString(cursor.getColumnIndex(BookmarkColumns.URL));
-			startService(getIntent().<Intent>getParcelableExtra("SEND_MESSAGE").setAction(title + ": " + url));
-			finish();
+			int count = 0;
+			Pair[] pairs = new Pair[3];
+			while (cursor.isAfterLast() == false && count < 3) {
+				String title = cursor.getString(cursor.getColumnIndex(BookmarkColumns.TITLE));
+				String url = cursor.getString(cursor.getColumnIndex(BookmarkColumns.URL));
+				Pair<String, String> p = new Pair<String, String>(title, url);
+				pairs[count] = p;
+				cursor.moveToNext();
+				count++;
+			}
+			TextView tv = (TextView) findViewById(R.id.textView8);
+			tv.setText(pairs[0].first + ": " + pairs[0].second);
+			tv = (TextView) findViewById(R.id.textView9);
+			tv.setText(pairs[1].first + ": " + pairs[1].second);
+			tv = (TextView) findViewById(R.id.textView10);
+			tv.setText(pairs[2].first + ": " + pairs[2].second);
+//			startService(getIntent().<Intent>getParcelableExtra("SEND_MESSAGE").setAction(title + ": " + url));
+//			finish();
 		}
 	}
 
